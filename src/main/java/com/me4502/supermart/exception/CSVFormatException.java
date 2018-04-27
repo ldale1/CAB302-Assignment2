0f25829fb6e1db3f8540abbdb34077b8ecae9b67
Matthew Miller <mnmiller1@me.com>
2018-04-27 15:50:31 +1000

    Setup initial project files

---
+++ src/main/java/com/me4502/supermart/exception/CSVFormatException.java
@@ -0,0 +1,10 @@
+package com.me4502.supermart.exception;
+
+/**
+ * Indicates that the CSV Format was invalid in some way.
+ *
+ * @author Matthew Miller
+ */
+public class CSVFormatException extends IllegalArgumentException {
+
+}
---
+++ src/main/java/com/me4502/supermart/exception/DeliveryException.java
@@ -0,0 +1,10 @@
+package com.me4502.supermart.exception;
+
+/**
+ * TODO
+ *
+ * @author Matthew Miller
+ */
+public class DeliveryException extends Exception {
+
+}
